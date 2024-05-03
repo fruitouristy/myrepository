62893038164cb01c0c1e728f87413298efa9877b
fruitourist@gmail.com <fruitouristy@yandex.ru>
2024-05-03 19:52:50 +0400

    2.7

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('mymethod', views.mymethod)
+    path('', views.index)
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+
 
 def index(request):
     return render(request, 'index.html')
-
-@csrf_exempt
-def mymethod(request):
-    print(request.method)
-    return HttpResponse()
